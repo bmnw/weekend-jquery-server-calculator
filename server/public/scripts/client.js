97ede6cbdc5f1dfe10c20b497b9c53107869ba6e
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-07-25 17:46:41 -0500

    added click handler for clear input button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,17 @@ $(readyNow);
 
 function readyNow() {
     console.log('ready now');
+    $('#clear-inputs').on('click', clearInputs);
+}
+
+/**
+ * Clears values that the user entered into the input fields
+ */
+
+function clearInputs() {
+    console.log('in clearInputs');
+    $('#input-one').val(''); // empty first input, set val to empty string
+    $('#input-two').val(''); // empty second input, set val to empty string
+    console.log('input 1', $('#input-one').val());
+    console.log('input 2', $('#input-two').val());
 }
