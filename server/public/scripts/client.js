b6bf6044346e7b774a293e5906602dfd67768842
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-07-27 12:47:23 -0500

    edited code based onwhat we covered in lecture last night regarding get and post requests; issue: req.body is undefined in the server console log, the post request data is not being read by the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,7 +5,7 @@ $(readyNow);
 function readyNow() {
     console.log('ready now');
     $('.operator').on('click', captureMathOperator);
-    $('#equals').on('click', postNewEquation);
+    $('#equation-submit').on('click', sendEquationToServer);
     $('#clear-inputs').on('click', clearInputs);
 }
 
@@ -21,23 +21,29 @@ function clearInputs() {
     console.log('input 2', $('#input-two').val());
 }
 
-let inputOne;
-let operator;
-let inputTwo;
+// let inputOne;
+// let operator;
+// let inputTwo;
 
-/**
- * Pushes new equation object to the server
- */
-
-function postNewEquation() {
-    console.log('in postNewEquation');
-    inputOne = $('#input-one').val(); // get the first input number
-    operator = currentOperator; // get the math operator
-    inputTwo = $('#input-two').val(); // get the second input number
-    console.log('input one, operator, input two:', inputOne, operator, inputTwo);
-    newEquation(inputOne, operator, inputTwo);
-    // postRequest();
-    getEquations();
+function sendEquationToServer() {
+    console.log('in sendEquationToServer');
+    console.log('input one:', $('#input-one').val());
+    console.log('math operator:', currentOperator);
+    console.log('input two:', $('#input-two').val());
+    $.ajax({
+        type: 'POST',
+        url: '/equations',
+        data: {
+            inputOne: $('#input-one').val(),
+            mathOperator: currentOperator,
+            inputTwo: $('#input-two').val()
+        }
+    }).then(function (response){
+        getEquations();
+    }).catch(function (error){
+        console.log(error);
+        alert('Something went wrong. Please try again.');
+    });
 }
 
 function getEquations() {
@@ -47,55 +53,35 @@ function getEquations() {
         url: '/equations'
     }).then(function(response) {
         console.log('response:', response);
+        $('#equation-history').empty();
         for(let equation of response){
             $('#equation-history').append(`
                 <p>
                     ${equation.inputOne} ${equation.mathOperator} ${equation.inputTwo}
                 </p>
             `);
-            console.log(typeof equation.operator);
         }
     }).catch(function(error) {
         console.log(error);
-        alert('something went wrong, please try again.');
+        alert('Something went wrong. Please try again.');
     });
 }
-
-// function postRequest() {
-//     console.log('in postRequest');
-//     // making a post request to the server
-//     $.ajax({
-//         method: 'POST', // type of request
-//         url: '/equation', // route to be matched on the server
-//         data: {
-//             equationToAdd: 
-//                 newEquation(inputOne, operator, inputTwo)
-//             }
-//     }).then(function(response){
-//         console.log('successful response');
-//         // refresh equation history
-//         getEquations(); // need to declare this function
-//     }).catch(function(response){
-//         // alert user to a failed response
-//         alert('something went wrong, please try again');
-//     })
-// }
 
 /**
  * Creates new equation object
  */
 
-function newEquation(input1, operator, input2) {
-    console.log('in newEquation');
-    // create equation object with the input numbers and selected operator as properties
-    let newEquation = {
-        inputOne: input1,
-        mathOperator: operator,
-        inputTwo: input2
-    }
-    console.log('equation object:', newEquation);
-    return newEquation;
-}
+// function newEquation(input1, operator, input2) {
+//     console.log('in newEquation');
+//     // create equation object with the input numbers and selected operator as properties
+//     let newEquation = {
+//         inputOne: input1,
+//         mathOperator: operator,
+//         inputTwo: input2
+//     }
+//     console.log('equation object:', newEquation);
+//     return newEquation;
+// }
 
 let currentOperator;
 
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static('server/public'));
 
-const equationHistory = [
+const equations = [
     {
         inputOne: 12,
         mathOperator: '+',
@@ -15,12 +15,16 @@ const equationHistory = [
 ];
 
 app.get('/equations', (req, res) => {
-    res.send(equationHistory);
+    res.send(equations);
 });
 
-// app.post('/equation', (req, res) => {
-
-// });
+app.post('/equations', (req, res) => {
+    const equationToAdd = req.body;
+    console.log(req.body);
+    equations.push(equationToAdd);
+    res.sendStatus(200);
+    // res.send(equation);
+});
 
 app.listen(port, () => {
     console.log('listening on port', port);
